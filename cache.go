9b0645e4456c839b35ff0b26a272d1c97c572a41
Akbarkhon Amirkhonov <8295502+amirkhonov@users.noreply.github.com>
2022-06-17 10:17:37 +0500

    test

--- cache.go
+++ cache.go
@@ -2,22 +2,63 @@ package cache
 
 import "time"
 
+type cacheRecord struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	data map[string]cacheRecord
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{data: make(map[string]cacheRecord)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	value, ok := c.data[key]
 
+	if !ok {
+		return "", false
+	}
+
+	if isExpired(value.deadline, time.Now()) {
+		delete(c.data, key)
+		return "", false
+	}
+
+	return value.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = cacheRecord{
+		value:    value,
+		deadline: time.Time{}, // zero value for Time
+	}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0, len(c.data))
+	now := time.Now()
+
+	for key, value := range c.data {
+		if isExpired(value.deadline, now) {
+			delete(c.data, key)
+		} else {
+			keys = append(keys, key)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = cacheRecord{
+		value:    value,
+		deadline: deadline,
+	}
+}
+
+func isExpired(time, now time.Time) bool {
+	return !time.IsZero() && (time.Before(now) || time.Equal(now))
 }
